73030968fcb69ff17550dbd8432287a0416f77de
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2020-06-02 01:39:20 +0300

    Added a LESS file for theme variables and changed the default looks.

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,34 @@
 </template>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "./cssresources/theme.less";
+
+* {
+    color: @theme-text-color;
+    background: @theme-background;
+}
+
+html, body, #app {
+    width: 100vw;
+    height: 100vh;
+    padding: 0;
+    margin: 0;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
