133c5e59e9bd34d0561688261d17092ae5c25f80
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2020-06-02 03:19:13 +0300

    Implemented the skeleton of the timeline component.

---
+++ src/components/Timeline.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="timeline-container">
+      <div class="mark" v-for="markTime in ~~numberOfMarks + 1" :key="markTime">
+            {{ ((~~markTime-1)/~~numberOfMarks) * (~~rangeEnd - ~~rangeStart) }}
+            <div class="vertical-line"></div>
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+export enum TimelineLook {
+    Line,
+    Audiowave
+};
+
+@Component
+export default class Timeline extends Vue {
+    @Prop()
+    private look!: TimelineLook;
+
+    @Prop()
+    private rangeStart!: number;
+
+    @Prop()
+    private rangeEnd!: number;
+
+    @Prop()
+    private numberOfMarks!: number;
+
+    public functionToSilenceWarnings (): void {
+      console.log(this.look === TimelineLook.Line);
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+@import "../cssresources/theme.less";
+
+.timeline-container {
+    width: 100%;
+    height: 150px;
+    background: @theme-focus-color;
+    border: 2px solid @theme-border-color;
+    margin-bottom: 2%;
+    display: flex;
+    justify-content: space-between;
+}
+
+.mark {
+    display: fle;
+}
+
+.vertical-line {
+    height: 30%;
+    width: 10px;
+    background: @theme-background;
+}
+</style>
---
+++ src/components/TimelinePlayer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="timeline-player">
+    <audio nocontrols></audio>
+    <Timeline look={TimelineLook.Line} numberOfMarks=10 rangeStart=0 rangeEnd=60></Timeline>
+    <Timeline look={TimelineLook.Audiowave} numberOfMarks=10 rangeStart=0 rangeEnd=60></Timeline>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { default as Timeline, TimelineLook } from "./Timeline.vue";
+
+@Component({
+  components: { Timeline }
+})
+export default class TimelinePlayer extends Vue {
+    @Prop() private msg!: string;
+};
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+@import "../cssresources/theme.less";
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
