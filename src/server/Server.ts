241f1432b9c52d7880386438741689238f0e894a
Nikola Dimitroff <dimitroff.nikola@gmail.com>
2020-08-04 23:47:31 +0300

    Added a basic server route for comments.

---
+++ src/server/Server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+
+import CommentController from "./CommentController";
+import bodyParser from "body-parser";
+import CommonParams from "../logic/CommonParams";
+
+export default class Server {
+    private app: express.Application;
+
+    constructor() {
+        this.app = express();
+        this.app.use(bodyParser.json());
+    }
+
+    public init(): void {
+        CommentController.registerRoutes(this.app);
+
+        this.app.listen(CommonParams.APIServerPort, CommonParams.APIServerIP, () => {
+            console.log(`API server is listening on http://localhost:${CommonParams.APIServerPort}`);
+        });
+    }
+}
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,19 +1,5 @@
-import express, { Application } from "express";
-import bodyParser from "body-parser";
+import Server from "./Server";
 
-import CommonParams from "../logic/CommonParams";
+const server: Server = new Server();
 
-const app: Application = express();
-
-// parse application/json
-app.use(bodyParser.json());
-
-app.use(function (req, res) {
-    res.setHeader("Content-Type", "text/plain");
-    res.write("you posted:\n");
-    res.end(JSON.stringify(req.body, null, 2));
-});
-
-app.listen(CommonParams.APIServerPort, CommonParams.APIServerIP, () => {
-    console.log(`API server is listening on http://localhost:${CommonParams.APIServerPort}`);
-});
+server.init();
